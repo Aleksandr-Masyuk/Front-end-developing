33d1d8e6c299196f0e88d795feb0d1aefc10bb7b
Aleksandr-Masyuk <79687501+Aleksandr-Masyuk@users.noreply.github.com>
2021-07-07 16:24:26 +0300

    Delete lessons Two-4 js directory

--- lessons Two-4 js/69 обфускатор/js.js
+++
@@ -1,8 +0,0 @@
-// let host = window.location.hostname;
-// console.log(host);
-// if (host != "js.js:2 127.0.0.1" ) {
-//     location.href = " "; //можно выолнять любые действия
-// }
-
-
-eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6 3=7.4.8;9.a(3);b(3!="5.5:2 c.0.0.1"){4.d="e://f.g.h/"}',18,18,'|||host|location|js|let|window|hostname|console|log|if|127|href|https|forum|icoder|uz'.split('|'),0,{}))
